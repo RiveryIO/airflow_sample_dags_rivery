d9a32975ae807af84ad6577d5eae7027f71eff2c
Ujjwal Tamhankar <utamhank1@gmail.com>
2021-10-27 15:30:42 -0400

    Modified dag to compile and appear in the airflow webserver UI

--- river_run/RiveryAPI_airflow_dag_example.py
+++ river_run/RiveryAPI_airflow_dag_example.py
@@ -1,43 +1,33 @@
 # Rivery API Airflow DAG example.
 
 # Import libraries.
- from datetime import timedelta
+from datetime import datetime, timedelta
+import airflow
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
 
- import airflow
- from airflow.operators import DAG
- from airflow.operators.https_operator import SimpleHttpOperator
- from airflow.operators.bash_operator import BashOperator
-
- default_args = {
- 	'owner': 'airflow',
- 	'depends_on_past': 'False',
- 	'start_date': airflow.utils.dates.days_ago(0),
- 	'email_on_failure': True,
- 	'email_on_retry': False,
- 	'retries': 1,
- 	'retry_delay': timedelta(minutes=1)
- }
+default_args = {
+ 'owner': 'airflow',
+ 'depends_on_past': 'False',
+ 'start_date': airflow.utils.dates.days_ago(0),
+ 'email_on_failure': True,
+ 'email_on_retry': False,
+ 'retries': 1,
+ 'retry_delay': timedelta(minutes=1)
+}
 
 dag = DAG('airflow_rivery_api_test',
     schedule_interval='@once',
     default_args=default_args)
 
-t1 = BashOperator(
-	bash_command)
-
-t1 = SimpleHttpOperator(
-    task_id='test_initiate_river',
-    method='POST',
-    http_conn_id='https://',
-    endpoint='console.rivery.io/#/river/55bf7c4270fdca16cac18761/river/5f2d75c4f5682c764a8da7c1',
-    headers={"accept": "application/json",
-    		 "Authorization": "Bearer <your_api_token>",
-    		 "Content-Type": "application/json"},
-    dag=dag)
+start = DummyOperator(
+	task_id="airflow_rivery_api_test",
+	dag=dag)
 
 t1 = BashOperator(
 	task_id= 'test_initiate_river',
-	start_date= datetime.datetime.now(),
+	start_date= datetime.now(),
 	bash_command= "curl -X POST “https://console.rivery.io/api/run\” "
 
 					"-H \“accept: application/json\”"
@@ -46,6 +36,7 @@ t1 = BashOperator(
 
 					"-H \“Content-Type: application/json\”"
 
-					"-d \"{ \\\“river_id\\\”: \\\"<your_river_id>\\\"}\"",")")
+					"-d \"{ \\\“river_id\\\”: \\\"<your_river_id>\\\"}\")"
+				)
 
 start >> [t1]

27764cfcac4f5085bd792217c2b5ff063cdfe9d7
Ujjwal Tamhankar <utamhank1@gmail.com>
2021-10-29 17:42:49 -0400

    Initial commit

--- download_activity_logs/RiveryAPI_airflow_download_activity.py
+++ download_activity_logs/RiveryAPI_airflow_download_activity.py
@@ -16,7 +16,7 @@ with DAG(
     tags=["example"],
 ) as dag:
 
-	def check_river(**kwargs):
+	def download_activity_logs_initial_request(**kwargs):
 		"""Import libraries"""
 		import requests
 		import json
@@ -36,16 +36,44 @@ with DAG(
 		response = requests.request("GET", url, headers=headers, data=payload)
  
 		print(response.text)
-		return response.json()['river_run_status']
+		return response.json()['queryId']
+
+	def download_activity_logs_second_request(**kwargs):
+		"""Import libraries"""
+		import requests
+		import json
+		
+		"""Parse configuration variables."""
+		run_id = kwargs['dag_run'].conf['run_id']
+		access_token = kwargs['dag_run'].conf['access_token']
+		
+		"""Make HTTP request and log the response."""
+		url = f"https://console.rivery.io/api/check_run?run_id={run_id}"
+		 
+		payload={}
+		headers = {
+		 'Authorization': f"Bearer {access_token}"
+		}
+		 
+		response = requests.request("GET", url, headers=headers, data=payload)
+ 
+		print(response.text)
+		return response.text
 
 
 	"""Create dummy operator for dag initiation."""
-	start = DummyOperator(task_id="Initiate_Dag", dag=dag)
+	start = DummyOperator(task_id="download_activity_log", dag=dag)
 
-	run_this = PythonOperator(
-		task_id="Check_run_status",
-		python_callable=check_river,
+	initial_request = PythonOperator(
+		task_id="first_request",
+		python_callable=download_activity_logs,
+	)
+
+	second_request = PythonOperator(
+		task_id="second_request",
+		python_callable=download_activity_logs_second_request,
 	)
 
 	"""Execute DAG."""
-	run_this.set_upstream(start)
+	second_request.set_upstream(first_request)
+	initial_request.set_upstream(start)
